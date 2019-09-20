b3153f0e5d2ede5818d21273f2031d61ab434b7f
Neil Huang <neil01428@gmail.com>
2019-09-20 09:45:35 +0800

    Add yolo

---
+++ 3.YoloV3/darknet-master/Deepfashion/voc/VOCdevkit/VOC2007/test.py
@@ -0,0 +1,37 @@
+import os
+import random
+
+trainval_percent = 0.3
+train_percent = 0.7
+xmlfilepath = 'Annotations'
+txtsavepath = 'ImageSets/Main'
+total_xml = os.listdir(xmlfilepath)
+
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+
+ftrainval = open('ImageSets/Main/trainval.txt', 'w')
+ftest = open('ImageSets/Main/test.txt', 'w')
+ftrain = open('ImageSets/Main/train.txt', 'w')
+fval = open('ImageSets/Main/val.txt', 'w')
+
+for i in list:
+    if total_xml[i].split(".")[-1] !="DS_Store":
+        name = total_xml[i][:-4] + '\n'
+        if i in trainval:
+            ftrainval.write(name)
+            if i in train:
+                ftest.write(name)
+            else:
+                fval.write(name)
+        else:
+            ftrain.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
