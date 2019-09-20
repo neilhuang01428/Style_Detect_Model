3a3317d1a210a84f54a68896b0105682f688a4b7
Neil Huang <neil01428@gmail.com>
2019-09-20 10:42:59 +0800

    Change API key

--- 5.Line_bot/config/config.py
+++ 5.Line_bot/config/config.py
@@ -1,6 +1,6 @@
 #domain url
-DOMAIN_URL = 'https://2bd1295b.ngrok.io'
+DOMAIN_URL = 'https://xxxx.ngrok.io'
 
 #Line
-CHANNEL_SECRET = "917ad10fe2b9dc5bff8e47a8fa70b919"
-CHANNEL_ACCESS_TOKEN = "jDyA7TarxKKEikCvP0ROi4rWVGdBrZl5FctEq6xZitqfYonJxeuma2yVV1f/DruwqBZqArCIIn3Mj53FDmAgJlEC4CpXaineRaOvmO7uxWtfSAPc2yrqibM3rd/ghdeGpnd0oV4dObNXQ2zThOk/ywdB04t89/1O/w1cDnyilFU="
+CHANNEL_SECRET = "xxxx"
+CHANNEL_ACCESS_TOKEN = "xxxx"
